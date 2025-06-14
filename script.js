d7ef5fcea0fedb35682377bc6dbf1c4ab843f7fe
Yohan Quraishy <116612238+Yohan-Abraham@users.noreply.github.com>
2025-06-14 13:04:57 -0700

    Add Buttons And Store Choice
    Added buttons for player to make choice, and stored their choice.

--- script.js
+++ script.js
@@ -8,66 +8,75 @@ function getComputerChoice() {
         return "paper"
 }
 
-function getHumanChoice() {
-    input = prompt("Rock, paper, or scissors?");
-    return input;
-}
+function playRound(input, computer) {
 
-function playRound(human, computer) {
-
-    if (human.toLowerCase() === computer) {
+    if (input.toLowerCase() === computer) {
         console.log(`Its a Tie!`);
     }
 
-    else if (human.toLowerCase() === 'rock' && computer === 'paper') {
-        console.log(`Computer wins! ${computer} beats ${human}.`);
+    else if (input.toLowerCase() === 'rock' && computer === 'paper') {
+        console.log(`Computer wins! ${computer} beats ${input}.`);
         computerScore++;
     }
 
-    else if (human.toLowerCase() === 'rock' && computer === 'scissors') {
-        console.log(`You Win! ${human} beats ${computer}.`);
+    else if (input.toLowerCase() === 'rock' && computer === 'scissors') {
+        console.log(`You Win! ${input} beats ${computer}.`);
         humanScore++;
     }
 
-    else if (human.toLowerCase() === 'paper' && computer === 'rock') {
-        console.log(`You Win! ${human} beats ${computer}.`);
+    else if (input.toLowerCase() === 'paper' && computer === 'rock') {
+        console.log(`You Win! ${input} beats ${computer}.`);
         humanScore++;
     }
 
-    else if (human.toLowerCase() === 'paper' && computer === 'scissors') {
-        console.log(`Computer wins! ${computer} beats ${human}.`);
+    else if (input.toLowerCase() === 'paper' && computer === 'scissors') {
+        console.log(`Computer wins! ${computer} beats ${input}.`);
         computerScore++;
     }
 
-    else if (human.toLowerCase() === 'scissors' && computer === 'rock') {
-        console.log(`Computer wins! ${computer} beats ${human}.`);
+    else if (input.toLowerCase() === 'scissors' && computer === 'rock') {
+        console.log(`Computer wins! ${computer} beats ${input}.`);
         computerScore++;
     }
 
     else {
-        console.log(`You Win! ${human} beats ${computer}.`);
+        console.log(`You Win! ${input} beats ${computer}.`);
         humanScore++;
     }
 
 }
 
-function playGame() {
-    for (let i = 1; i <= 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
 
-    if (humanScore > computerScore) {
-        console.log(`You Win! Your Score: ${humanScore}.`);
-    }
-    else if (computerScore > humanScore) {
-        console.log(`Computer Wins! Computer Score: ${computerScore}.`);
-    }
-    else {
-        console.log(`Its a Tie! Your Score: ${humanScore}\nComputer Score ${computerScore}.`);
-    }
-}
+
+
 
 humanScore = 0;
 computerScore = 0;
 
-playGame();
+
+const buttons = document.querySelectorAll("button");
+
+let input;
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        input = btn.textContent;
+        playRound(input, getComputerChoice());
+    });
+});
+
+// function playGame() {
+//     for (let i = 1; i <= 5; i++) {
+//         playRound(getHumanChoice(), getComputerChoice());
+//     }
+
+//     if (humanScore > computerScore) {
+//         console.log(`You Win! Your Score: ${humanScore}.`);
+//     }
+//     else if (computerScore > humanScore) {
+//         console.log(`Computer Wins! Computer Score: ${computerScore}.`);
+//     }
+//     else {
+//         console.log(`Its a Tie! Your Score: ${humanScore}\nComputer Score ${computerScore}.`);
+//     }
+// }
